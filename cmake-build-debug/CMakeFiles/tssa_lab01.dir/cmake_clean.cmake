b3e771db9008cb7f9085e3ae786ce3d55e027589
zhenerenya <zhenyachmchk@gmail.com>
2020-10-11 19:22:48 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/tssa_lab01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tssa_lab01.dir/main.cpp.o"
+  "tssa_lab01"
+  "tssa_lab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tssa_lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
