21fd3fec82c9ba5e0992f276189b8abeb8d16cba
zhenerenya <zhenyachmchk@gmail.com>
2020-12-19 20:23:04 +0300

    add file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <iomanip>
 
+#include <map>
+
 double function (double& x){
     return exp(-0.2*x)*sin(x)+1;
 }
@@ -37,69 +39,83 @@ int main() {
 
     //Fibonachi
     std::cout<<std::endl<<std::endl<<"PHIBONACHI"<<std::endl;
-    int N;
-    std::cout<<"Type N"<<std::endl;
-    std::cin>>N;
+    int N=1;
 
-   std::cout<<"| i  ||"<<std::setw(19)<<"   Result    "<<std::setw(22)<<"||"<<std::setw(33)<<"   Partition     "<<std::setw(16)<<"    |"<<std::endl;
-   std::cout<<"|    ||"<<std::setw(3)<<"   x"<<"      |"<<std::setw(6)<<"f(x)"<<"      |"<<
-   std::setw(15)<<"precision"<<" ||"<<std::setw(7)<<"left"<< "    |"<<std::setw(7) <<"right"<<
-   "    |"<<std::setw(10)<<" f(left)"<<" |"<<std::setw(7)<<" f(right)"<<"  |"<<std::endl;
-   std::cout<<std::setfill('-')<<std::setw(97)<<" ";
-   std::cout<<std::endl;
-   std::cout << std::setfill(' ');
+    bool flag=1;
+    while (flag) {
+        std::cout << "| i  ||" << std::setw(19) << "     Result" << std::setw(24) << "||" << std::setw(33)
+                  << "  Partition" << std::setw(15) << "|" << std::endl;
+        std::cout << "|    ||" << std::setw(3) << "   x" << "      |" << std::setw(6) << "f(x)" << "       |" <<
+                  std::setw(15) << "precision" << " ||" << std::setw(7) << "left" << "    |" << std::setw(7) << "right"
+                  <<
+                  "    |" << std::setw(10) << " f(left)" << " |" << std::setw(7) << " f(right)" << "  |" << std::endl;
+        std::cout << std::setfill('-') << std::setw(97) << " ";
+        std::cout << std::endl;
+        std::cout << std::setfill(' ');
 
-   double A=a;
-   double B=b;
-   double d=(B-A)/2.;
-   double _x1; //первая внутренняя точка
-    double x1 = a + F(N - 2) * 1. / F(N) * (b - a);
-    double x2 = a + F(N - 1) * 1. / F(N) * (b - a);
-    double f_1=function(x1);
-    double f_2=function(x2);
-    double x=(B-A)/2.+A;
-    std::cout<<"|  1 || "<<std::setw(5)<<x1<<"  |  "<<function(x)<< "  |"<<std::setw(15)<<d
-    <<" ||  "<<std::setw(7) <<A<<"  |  "<<std::setw(7)<<B<<"  | " <<f_1<<"  | "<<f_2<<"  |"<<std::endl;
+        double A = a;
+        double B = b;
+        double d = (B - A) / 2.;
+        double _x1; //первая внутренняя точка
+        double x1 = a + F(N - 2) * 1. / F(N) * (b - a);
+        double x2 = a + F(N - 1) * 1. / F(N) * (b - a);
+        double f_1 = function(x1);
+        double f_2 = function(x2);
+        double x = (B - A) / 2. + A;
+        std::cout << "|  1 || " << std::setw(5) << x1 << "  |  " << function(x) << "  |" << std::setw(15) << d
+                  << "||  " << std::setw(7) << A << "    |  " << std::setw(7) << B << "  | " << f_1 << "  | " << f_2
+                  << "  |" << std::endl;
 
-    if(function(x1)<function(x2)){
-        B=x2;
-        _x1=x1;
-    } else {
-        A=x1;
-        _x1=x2;
-    }
-
-    for (int i=2; i<=N; i++) {
-       double _x2;
-       x=(B-A)/2.+A;
-       double d=x-_x1;
-       _x2=x+d;
-       if(_x1<_x2){ //где правая и где левая точка
-           x1=_x1;
-           x2=_x2;
-       } else {
-           x1=_x2;
-           x2=_x1;
-       }
-       f_1=function(x1);
-       f_2=function(x2);
-       d=(B-A)/2.;
-        std::cout<<"|"<<std::setw(3)<<i<<" || "<<std::setw(7)<<x1<<"  |  "<<function(x)<< "  |  "<<std::setw(12)<<d
-                 <<"  ||  " <<A<<"  |  "<<std::setw(7)<<B<<"  | " <<std::setw(8)<<f_1<<"  | "<<std::setw(8)<<f_2<<"  |"<<std::endl;
-
-        if(function(x1)<function(x2)){
-            B=x2;
-            _x1=x1;
-            f_min=f_1;
+        if (function(x1) < function(x2)) {
+            B = x2;
+            _x1 = x1;
         } else {
-            A=x1;
-            _x1=x2;
-            f_min=f_2;
+            A = x1;
+            _x1 = x2;
         }
 
-   }
+        for (int i = 2; i <= N; i++) {
+            double _x2;
+            x = (B - A) / 2. + A;
+            double d = x - _x1;
+            _x2 = x + d;
+            if (_x1 < _x2) { //где правая и где левая точка
+                x1 = _x1;
+                x2 = _x2;
+            } else {
+                x1 = _x2;
+                x2 = _x1;
+            }
+            f_1 = function(x1);
+            f_2 = function(x2);
+            d = (B - A) / 2.;
+            std::cout << "|" << std::setw(3) << i << " || " << std::setw(7) << x1 << "  |  " << function(x) << "  |  "
+                      << std::setw(13) << d
+                      << "  ||  " << A << "  |  " << std::setw(7) << B << "  | " << std::setw(8) << f_1 << "  | "
+                      << std::setw(8) << f_2 << "  |" << std::endl;
 
-    std::cout<<"FUNCTION_min = "<<f_min<<" in point x="<<x<<"+-"<<(B-A)/2.<<std::endl;
+            if (function(x1) < function(x2)) {
+                B = x2;
+                _x1 = x1;
+                f_min = f_1;
+            } else {
+                A = x1;
+                _x1 = x2;
+                f_min = f_2;
+            }
+
+        }
+        if ((B-A)/2. <= e / 2) {
+            std::cout << "FUNCTION_min = " << f_min << " in point x=" << x << "+-" << (B - A) / 2. << std::endl;
+            flag=false;
+        } else {
+            std::cout << "D>e. N++" << std::endl;
+            N++;
+            flag=true;
+        }
+    }
+
+
 
 
 
